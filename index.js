871c0bafca9c736c9ca71a9df383469304936fd8
Noah Puckett <noah.ingram.puckett@gmail.com>
2019-06-26 15:08:32 -0700

    created tests, modified request routers...responders? whatever they are called

--- index.js
+++ index.js
@@ -1,38 +1,5 @@
-const { createServer } = require('http');
-const fs = require('fs');
+const app = require('./lib/app');
 
-//req is the request OBJECT
-
-//res is the RESPONSE object
-
-const server = createServer((req, res) => {
-    console.log('Client side made a request');
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-
-    res.end(`<!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>Success!</title>
-        <style>
-        li { color: red }
-        </style>
-    </head>
-    <body>
-        <h1>YOU HAVE SUCCEEDED!</h1>
-        <ul>
-            <li>SO GOOD</li>
-            <li>REALLY</li>
-            <li>KILLIN IT</li>
-        </ul>
-        <script>console.log('SUUUUUP')</script>
-    </body>
-    </html>`);
-});
-
-server.listen(8080, () => {
+app.listen(8080, () => {
     console.log('Listening on port 8080');
 });
---
+++ tests/app.test.js
@@ -0,0 +1,38 @@
+const app = require('../lib/app');
+const request = require('supertest');
+
+describe('application path routing', () => {
+
+    it('returns welcome if path is /', () => {
+        return request(app)
+            .get('/')
+            .then(res => {
+                expect(res.text).toEqual(expect.stringContaining('<h1>*flicks light switch rapidly* WELCOME TO HELL WELCOME TO HELL WELCOME TO HELL</h1>'))
+            })
+    }); 
+
+    it('returns red html when path is /red', () => {
+        return request(app)
+            .get('/red')
+            .then(res => {
+                expect(res.text).toEqual(expect.stringContaining('<style>span { color: green}</style><h1>WELCOME TO THE <span>RED</span> ZONE</h1>'))
+            })
+    }); 
+
+    it('returns blue html when path is /red', () => {
+        return request(app)
+            .get('/blue')
+            .then(res => {
+                expect(res.text).toEqual(expect.stringContaining('<style>span { color: red}</style><h1>WELCOME TO THE <span>BLUE</span> ZONE</h1>'))
+            })
+    }); 
+
+    it('returns green html when path is /red', () => {
+        return request(app)
+            .get('/green')
+            .then(res => {
+                expect(res.text).toEqual(expect.stringContaining('<style>span { color: blue}</style><h1>WELCOME TO THE <span>GREEN</span> ZONE</h1>'))
+            })
+    }); 
+
+});
