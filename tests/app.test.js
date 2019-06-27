c16489aed67c88207ec55561c20529f8c88837a8
Noah Puckett <noah.ingram.puckett@gmail.com>
2019-06-26 22:48:32 -0700

    added tests, neither pass and I dont know whyyyy

--- tests/app.test.js
+++ tests/app.test.js
@@ -3,49 +3,55 @@ const request = require('supertest');
 
 describe('application path routing', () => {
 
-    it('returns welcome if path is /', () => {
+    //this fails, whyyy
+    it('returns an error if wrong path is given', () => {
         return request(app)
-            .get('/')
-            .then(res => {
-                expect(res.status).toEqual(200);
-                expect(res.text).toEqual(expect.stringContaining('<h1>*flicks light switch rapidly* WELCOME TO HELL WELCOME TO HELL WELCOME TO HELL</h1>'));
-            })
-    }); 
-
-    it('returns red html when path is /red', () => {
-        return request(app)
-            .get('/red')
-            .then(res => {
-                expect(res.status).toEqual(200);
-                expect(res.text).toEqual(expect.stringContaining('<style>span { color: green}</style><h1>WELCOME TO THE <span>RED</span> ZONE</h1>'));
-            })
-    }); 
-
-    it('returns blue html when path is /red', () => {
-        return request(app)
-            .get('/blue')
-            .then(res => {
-                expect(res.status).toEqual(200);
-                expect(res.text).toEqual(expect.stringContaining('<style>span { color: red}</style><h1>WELCOME TO THE <span>BLUE</span> ZONE</h1>'));
-            })
-    }); 
-
-    it('returns green html when path is /red', () => {
-        return request(app)
-            .get('/green')
-            .then(res => {
-                expect(res.status).toEqual(200);
-                expect(res.text).toEqual(expect.stringContaining('<style>span { color: blue}</style><h1>WELCOME TO THE <span>GREEN</span> ZONE</h1>'));
-            })
-    }); 
-
-    it('returns a failure message and a status of 404', () => {
-        return request(app)
-            .get('/failure')
+            .get('/failure.html')
             .then(res => {
                 expect(res.status).toEqual(404);
-                expect(res.text).toEqual(expect.stringContaining('<h1>OH YOU\'VE DONE IT NOW!</h1>'));
+                expect(res.text).toEqual(expect.stringContaining('Oh now you\'ve done it!'));
             });
     }); 
+
+    //this fails and I ALSO don't know why
+    it('returns welcome header if path is /index.html', () => {
+        return request(app)
+            .get('/index.html')
+            .then(res => {
+                expect(res.status).toEqual(200);
+                expect(res.text).toBe(`
+                <!DOCTYPE html>
+                <html lang="en">
+                <head>
+                    <meta charset="UTF-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+                    <title>Document</title>
+                </head>
+                <body>
+                    <h1>WELCOME TO THE DAYDREAM
+                    </h1>
+                </body>
+                </html>`);
+            });
+    });
+
+    // it('returns welcome if path is /', () => {
+    //     return request(app)
+    //         .get('/')
+    //         .then(res => {
+    //             expect(res.status).toEqual(200);
+    //             expect(res.text).toEqual(expect.stringContaining('<h1>*flicks light switch rapidly* WELCOME TO HELL WELCOME TO HELL WELCOME TO HELL</h1>'));
+    //         });
+    // }); 
+
+    // it('returns a failure message and a status of 404', () => {
+    //     return request(app)
+    //         .get('/failure')
+    //         .then(res => {
+    //             expect(res.status).toEqual(404);
+    //             expect(res.text).toEqual(expect.stringContaining('<h1>OH YOU\'VE DONE IT NOW!</h1>'));
+    //         });
+    // }); 
 
 });
